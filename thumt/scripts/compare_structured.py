6123b0c69ad2d7ff67671bab99d504a7d2df07f0
Glaceon31 <grit31@126.com>
2019-02-18 17:46:37 +0800

    add processing scripts

---
+++ thumt/scripts/compare_structured.py
@@ -0,0 +1,110 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import operator
+import os
+
+import numpy 
+import time
+import math
+import json
+
+from calculate_oracle import bleu
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--src", type=str, required=True)
+    parser.add_argument("--hypo", type=str, required=True)
+    parser.add_argument("--baseline", type=str, required=True)
+    parser.add_argument("--compare", type=str)
+    parser.add_argument("--hidden", action="store_true")
+    #parser.add_argument("--oracle", type=str, required=True)
+    parser.add_argument("--refs", type=str, required=True, nargs="+")
+    parser.add_argument("--senid", type=int, default=-1)
+    return parser.parse_args()
+
+def rbpe(inp):
+    return inp.replace('@@ ', '')
+
+def getlines(inp):
+    result = inp.split('\n')
+    if result[-1] == '':
+        del result[-1]
+    return result
+
+def splitline(line):
+    result = line.split()
+    pos = 0
+    while pos < len(result):
+        if result[pos][0] == '<' and result[pos][-1] != '>':
+            if pos+1 < len(result):
+                result[pos] += ''+result[pos+1]
+                del result[pos+1]
+            else:
+                print('wrong!! ', result[pos])
+                exit()
+        else:
+            pos += 1
+    return result
+
+
+if __name__ == "__main__":
+    args = parseargs()
+
+    src = open(args.src, 'r').read() 
+    hypo = open(args.hypo, 'r').read() 
+    if args.compare:
+        compare = open(args.compare, 'r').read()
+    #oracle = open(args.oracle, 'r').read() 
+    baseline = open(args.baseline, 'r').read()
+    refs = [open(ref, 'r').read() for ref in args.refs]
+
+    if args.senid == -1:
+        lsrc = getlines(src)
+        lhypo = getlines(hypo)
+        lhypo = [' '.join(splitline(i)) for i in lhypo]
+        #loracle = getlines(oracle)
+        if args.compare:
+            lcompare = getlines(compare)
+            lcompare = [' '.join(splitline(i)) for i in lcompare]
+        lbase = getlines(baseline)
+        lrefs = [getlines(r) for r in refs]
+    else:
+        lsrc = [getlines(src)[args.senid]]
+        lhypo = [getlines(hypo)[0]]
+        if args.compare:
+            lcompare = [getlines(compare)[args.senid]]
+        #loracle = [getlines(oracle)[args.senid]]
+        lbase = [getlines(baseline)[args.senid]]
+        lrefs = [[getlines(r)[args.senid]] for r in refs]
+
+    print(len(lsrc))
+
+    worse = 0
+    better = 0
+    for i in range(len(lsrc)):
+        reftmp = [' '.join(splitline(t[i])) for t in lrefs]
+        if args.hidden and args.compare and lhypo[i] == lcompare[i]:
+            continue
+        print('=== %d ===' %i)
+        print('src:', lsrc[i])
+        print('ours:', lhypo[i], '('+str(bleu(rbpe(lhypo[i]), reftmp, 4, verbose=True))+')')
+        print('baseline:', lbase[i], '('+str(bleu(rbpe(lbase[i]), reftmp, 4, verbose=True))+')')
+        if args.compare:
+            print('compare:', lcompare[i], '('+str(bleu(rbpe(lcompare[i]), reftmp, 4, verbose=True))+')')
+        #print('oracle:', loracle[i], '('+str(bleu(rbpe(loracle[i]), reftmp, 4, verbose=True))+')')
+        if bleu(rbpe(lhypo[i]), reftmp, 4, verbose=True) > bleu(rbpe(lcompare[i]), reftmp, 4, verbose=True):
+            better += 1
+        elif bleu(rbpe(lhypo[i]), reftmp, 4, verbose=True) < bleu(rbpe(lcompare[i]), reftmp, 4, verbose=True):
+            worse += 1
+        for r in range(len(reftmp)):
+            print('ref'+str(r)+':', reftmp[r])
+        print('\n')
+        #if i == 100:
+        #    break
+    print('better:', better)
+    print('worse:', worse)
+
---
+++ thumt/scripts/preprocess_st_en.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import operator
+import os
+
+import numpy 
+import time
+import math
+import json
+import re
+import thulac
+
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input", type=str, required=True)
+    parser.add_argument("--bpedir", type=str, required=True)
+    parser.add_argument("--output", type=str, required=True)
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parseargs()
+    content = open(args.input, 'r').read()
+    lines = content.split('\n')
+    if lines[-1] == '':
+        del lines[-1]
+
+    middle = open('tmpprocess.in', 'w')
+    tags = []
+    segments = []
+    linenum = 0
+    for i in range(len(lines)):
+        line = lines[i]
+        tags_i = re.findall('<.*?>', line)
+        tags.append(tags_i)
+        tagre = re.compile('<.*?>')
+        segments_i = tagre.split(line)
+        for j in range(len(segments_i)):
+            if segments_i[j] == '':
+                segments_i[j] = -1
+            else:
+                middle.write(segments_i[j]+'\n')
+                segments_i[j] = linenum
+                linenum += 1
+        segments.append(segments_i)
+
+    middle.close()
+    ## preprocess
+    # normalize & tokenize & lowercase
+    os.system('python3 ~/utils/norm-char.py < tmpprocess.in > tmpprocess.norm')
+    os.system('perl ~/moses/mosesdecoder/scripts/tokenizer/tokenizer.perl -no-escape < tmpprocess.norm > tmpprocess.tok')
+    os.system('perl ~/moses/mosesdecoder/scripts/tokenizer/lowercase.perl < tmpprocess.tok > tmpprocess.out')
+
+    middle_out = open('tmpprocess.out', 'r')
+    lines_processed = middle_out.read().split('\n')
+    output = open(args.output, 'w')
+    for i in range(len(lines)):
+        segments_i = segments[i]
+        tags_i = tags[i]
+        result = ''
+        for j in range(len(segments_i)-1):
+            if segments_i[j] != -1:
+                result += ' '+lines_processed[segments_i[j]]
+            result += ' '+tags_i[j]
+        if segments_i[-1] != -1:
+            result += ' '+lines_processed[segments_i[-1]]
+        output.write(result.strip()+'\n')
+    output.close()
+
+    os.system('rm tmpprocess.in')
+    os.system('rm tmpprocess.norm')
+    os.system('rm tmpprocess.tok')
+    os.system('rm tmpprocess.out')
